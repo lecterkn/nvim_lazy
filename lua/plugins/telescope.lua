58dbbea4509233658734206ee4f5a56c7ea1d8d9
lecterkn <lecterkn@proton.me>
2025-01-21 11:21:04 +0900

    add telescope keybind

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,14 @@
 function config()
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
     vim.keymap.set("n", "<leader>ss", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>th", builtin.colorscheme, {})
 end
 
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = config
 }
