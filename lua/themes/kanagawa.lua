684518536935a4570a1e80b2d89468f3a4e92b95
lecterkn <lecterkn@proton.me>
2025-01-25 02:39:33 +0900

    kanagawa set to dragon

--- lua/themes/kanagawa.lua
+++ lua/themes/kanagawa.lua
@@ -4,13 +4,14 @@ return {
     "rebelot/kanagawa.nvim",
     config = function()
         require("kanagawa").setup({
-            theme = "wave",    -- Load "wave" theme when 'background' option is not set
-            background = {     -- map the value of 'background' option to a theme
-                dark = "wave", -- try "dragon" !
+            theme = "dragon",    -- Load "wave" theme when 'background' option is not set
+            background = {       -- map the value of 'background' option to a theme
+                dark = "dragon", -- try "dragon" !
                 light = "lotus"
             },
             keywordStyle = { italic = false },
             transparent = transparent,
+            terminalColors = true,
         })
     end
 }
