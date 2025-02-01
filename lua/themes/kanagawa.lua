77b1f2fbfb6966ae52fb88c09bb046889d0afd36
lecterkn <lecterkn@proton.me>
2025-02-02 02:37:18 +0900

    set kanagawa to 'wave'

--- lua/themes/kanagawa.lua
+++ lua/themes/kanagawa.lua
@@ -1,17 +1,17 @@
 local transparent = vim.g.transparent_enabled
 
 return {
-    "rebelot/kanagawa.nvim",
-    config = function()
-        require("kanagawa").setup({
-            theme = "dragon",    -- Load "wave" theme when 'background' option is not set
-            background = {       -- map the value of 'background' option to a theme
-                dark = "dragon", -- try "dragon" !
-                light = "lotus"
-            },
-            keywordStyle = { italic = false },
-            transparent = transparent,
-            terminalColors = true,
-        })
-    end
+	"rebelot/kanagawa.nvim",
+	config = function()
+		require("kanagawa").setup({
+			theme = "wave", -- "wave", "dragon", "lotus"
+			background = {
+				dark = "wave",
+				light = "lotus",
+			},
+			keywordStyle = { italic = false },
+			transparent = transparent,
+			terminalColors = true,
+		})
+	end,
 }
