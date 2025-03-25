c98643cf9ab813988278059b1e2c6fad0d8ef8b8
lecterkn <lecterkn@proton.me>
2025-03-26 03:11:07 +0900

    lsp settings for vue

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,52 +1,108 @@
 function MasonSetup()
-    local lspconfig = require("lspconfig")
+	local lspconfig = require("lspconfig")
 
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-        ensure_installed = {
-            "lua_ls",
-            "rust_analyzer",
-            "ts_ls",
-            "gopls",
-            "zls",
-            "tailwindcss",
-            "svelte",
-            "cssls",
-            "bashls",
-            "vimls",
-            "volar",
-        },
-        handlers = {
-            function(server_name)
-                lspconfig[server_name].setup({})
-            end,
-            ["gopls"] = function()
-                lspconfig.gopls.setup({
-                    settings = {
-                        gopls = {
-                            analyses = {
-                                unusedparams = true,
-                            },
-                            staticcheck = true,
-                            gofumpt = true,
-                        },
-                    },
-                })
-            end,
-            ["ts_ls"] = function()
-                lspconfig.ts_ls.setup({})
-            end,
-            ["volar"] = function()
-                lspconfig.volar.setup({})
-            end,
-        },
-    })
+	require("mason").setup()
+	require("mason-lspconfig").setup({
+		ensure_installed = {
+			"lua_ls",
+			"rust_analyzer",
+			"ts_ls",
+			"gopls",
+			"zls",
+			"tailwindcss",
+			"svelte",
+			"cssls",
+			"bashls",
+			"vimls",
+			"volar",
+		},
+		handlers = {
+			function(server_name)
+				lspconfig[server_name].setup({})
+			end,
+			["gopls"] = function()
+				lspconfig.gopls.setup({
+					settings = {
+						gopls = {
+							analyses = {
+								unusedparams = true,
+							},
+							staticcheck = true,
+							gofumpt = true,
+						},
+					},
+				})
+			end,
+			["ts_ls"] = function()
+				lspconfig.ts_ls.setup({
+					init_options = {
+						plugins = {
+							{
+								name = "@vue/typescript-plugin",
+								location = vim.fn.stdpath("data")
+									.. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+								languages = { "vue" },
+							},
+						},
+					},
+					settings = {
+						typescript = {
+							tsserver = {
+								useSyntaxServer = false,
+							},
+							inlayHints = {
+								includeInlayParameterNameHints = "all",
+								includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+								includeInlayFunctionParameterTypeHints = true,
+								includeInlayVariableTypeHints = true,
+								includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+								includeInlayPropertyDeclarationTypeHints = true,
+								includeInlayFunctionLikeReturnTypeHints = true,
+								includeInlayEnumMemberValueHints = true,
+							},
+						},
+					},
+				})
+			end,
+			["volar"] = function()
+				lspconfig.volar.setup({
+					init_options = {
+						vue = {
+							hybridMode = false,
+						},
+					},
+					settings = {
+						typescript = {
+							inlayHints = {
+								enumMemberValues = {
+									enabled = true,
+								},
+								functionLikeReturnTypes = {
+									enabled = true,
+								},
+								propertyDeclarationTypes = {
+									enabled = true,
+								},
+								parameterTypes = {
+									enabled = true,
+									suppressWhenArgumentMatchesName = true,
+								},
+								variableTypes = {
+									enabled = true,
+								},
+							},
+						},
+					},
+				})
+			end,
+		},
+	})
 end
 
 return {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-        "williamboman/mason.nvim",
-    },
-    config = MasonSetup,
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+	},
+	config = MasonSetup,
 }
